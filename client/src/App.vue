781a6288a79a05fcfaa3a705df72d75703818619
SyHe4 <143753565+SyHe4@users.noreply.github.com>
2023-10-18 04:25:47 -0400

    updated with vue

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue';
+</script>
+
+<template>
+  <header>
+    <NavBar />
+  </header>
+
+  <div class="container">
+    <RouterView />
+  </div>
+
+</template>
+
+<style scoped>
+  body {
+    background-color: azure;
+  }
