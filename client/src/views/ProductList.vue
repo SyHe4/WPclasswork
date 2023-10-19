a9a00e5a10ecd0fd65e68572d49a92fe470ba71a
SyHe4 <henixs1@newpaltz.edu>
2023-10-19 15:08:51 -0400

    flyout

--- client/src/views/ProductList.vue
+++ client/src/views/ProductList.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
-
+  import { ref } from 'vue'
+  import { getProducts, type Product} from '@/model/products';
+  const products = ref([] as Product[])
+  const isLoading = ref(false)
+  isLoading.value = true;
+  setTimeout(()  => {
+    products.value = getProducts()
+    isLoading.value - false;
+  }, 1000)
 </script>
 
 <template>
@@ -7,10 +15,41 @@
     <h1 class="title">
       Product List
     </h1>
+    <progress v-if="isLoading" class="progress is-success">Loading...</progress>
+    <div class="product-list">
+      <div v-for="product in products" :key="product.id" class="product"></div>
+      <img :src="product.thumbnail" :alt="product.title"/>
+      <h3 class="title is-3">{{ product.title }}</h3>
+      <p>{{ product.description }}</p>
+      <span>$</span>
+      <i class="price">{{ product.price }}</i>
+      <button class="button is-success">+</button>
+    </div>
   </div>
 </template>
 
 
 <style scoped>
+.product-list{
+  display: flex;
+  flex-wrap: wrap;
+  background-color: aliceblue;
+}
 
+.product {
+  flex-basis: 15rem;
+  flex-grow: 1;
+  margin: 1rem;
+  padding: .5rem;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: .5rem;
+  box-shadow: 0 0 1rem #0004; 
+}
+
+.price {
+  font-size: 2em;
+  font-weight: ;
+  margin-right: ;
+}
 </style>
