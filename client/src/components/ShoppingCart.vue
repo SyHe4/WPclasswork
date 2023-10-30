d1410101c4336dbe57bd2b56e1e6fcaa7d4f891a
SyHe4 <henixs1@newpaltz.edu>
2023-10-30 13:55:14 -0400

    shopping cart

--- client/src/components/ShoppingCart.vue
+++ client/src/components/ShoppingCart.vue
@@ -1,56 +1,63 @@
 <script setup lang="ts">
-import { type ShoppingCartItem, getCart, addToCart, removeFromCart, total, count } from "@/model/shoppingCart";
+import { type ShoppingCartItem, addToCart, getCart, removeFromCart, total, count } from "@/model/shoppingCart";
+
 const cart = getCart();
+
 </script>
 
 <template>
-  <div>
+  <div class="cart">
     <h1 class="subtitle">
-        Shopping Cart: {{ count }} items, total: ${{ total }}
+      Cart: {{ count }} items, total: ${{ total }}
     </h1>
+
     <div class="cart-items">
-        <div v-for="item in cart" :key="item.product.id" class="item">
-            <img src="item.product.thumbnail" :alt="item.product.title" />
-            <div class="item-details">
-                <h2 class="title is-5">{{ item.product.title }}</h2>
-                <p class="subtitle is-6">
-                    <select v-model="item.quantity">
-                        <option v-for="n in 10" :key="n" :value="n">{{ n }}</option>
-                    </select>
-                    {{ item.product.price }} x
-                    {{ item.quantity }} = ${{ item.product.price * item.quantity }}
-                    <button class="button is-danger is-small" @click="removeFromCart(item.product)">
-                        <div class="icon">
-                            <i class="fas fa-trash"></i>
-                        </div>
-                    </button>
-                </p>
-            </div>
-        </div>
+      <div v-for="item in cart" :key="item.product.id" class="item">
+        <img :src="item.product.thumbnail" :alt="item.product.title" />
+        <div class="item-details">
+          <h2 class="title is-5">{{ item.product.title }}</h2>
+          <p class="subtitle is-6">
+            <select v-model="item.quantity">
+              <option v-for="n in 10" :key="n" :value="n">{{ n }}</option>
+            </select>
+            x $ {{ item.product.price }}
+            = ${{ item.product.price * item.quantity }}
+            <button class="button is-danger is-small" @click="removeFromCart(item.product)">
+              <div class="icon">
+                <i class="fas fa-trash"></i>
+              </div>
+            </button>
+          </p>
+
+          </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.cart{
-    padding: 1rem;
-    overflow: scroll;
+.cart {
+  padding: 1rem;
+  overflow-y: scroll;
 }
-.item img{
-    width: 100px;
-    margin-right: .5rem;
+
+.cart-items {
+  display: flex;
+  flex-direction: column;
+
 }
-.cart-items{
-    display: flex;
-    flex-direction: column; 
+.item {
+  display: flex;
+  flex-direction: row;
+  margin: .5rem;
+  border: 1px solid #ccc;
+  border-radius: 1rem;
+  box-shadow: 0 0 1rem #0004;
+  overflow: hidden;
 }
-.item{
-    display: flex;
-    flex-direction: row;
-    margin: 5rem;
-    border: 1px solid white;
-    border-radius: 1rem;
-    box-shadow: 0 0 1rem #0004;
-    overflow: hidden;
+
+.item img {
+  width: 100px;
+  margin-right: .5rem;
 }
 </style>
--- client/src/views/ProductList.vue
+++ client/src/views/ProductList.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
-  import { getProducts, type Product} from '@/model/products';
-  import 
-  const products = ref([] as Product[])
-  const isLoading = ref(false)
-  isLoading.value = true;
-  setTimeout(()  => {
+import { getProducts, type Product } from '@/model/products';
+import { addToCart } from '@/model/shoppingCart';
+import { ref } from 'vue'
+
+const products = ref([] as Product[])
+const isLoading = ref(false)
+
+isLoading.value = true;
+setTimeout(()=> { 
     products.value = getProducts()
-    isLoading.value - false;
+    isLoading.value = false;
   }, 1000)
+
+
 </script>
 
 <template>
@@ -16,22 +20,27 @@
     <h1 class="title">
       Product List
     </h1>
-    <progress v-if="isLoading" class="progress is-success">Loading...</progress>
+    <progress v-if="isLoading" class="progress is-success" >Loading...</progress>
+
     <div class="product-list">
-      <div v-for="product in products" :key="product.id" class="product"></div>
-      <img :src="product.thumbnail" :alt="product.title"/>
-      <h3 class="title is-3">{{ product.title }}</h3>
-      <p>{{ product.description }}</p>
-      <span>$</span>
-      <i class="price">{{ product.price }}</i>
-      <button class="button is-success">+</button>
+      <div v-for="product in products" :key="product.id" class="product">
+        <img :src="product.thumbnail" :alt="product.title" />
+        <h3 class="title is-3">{{ product.title }}</h3>
+        <p>{{ product.description }}</p>
+        <p>
+          <span>$</span>
+          <i class="price">{{ product.price }}</i>
+          <button class="button is-success"  @click.prevent="addToCart(product)" >+</button>
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 
 <style scoped>
-.product-list{
+
+.product-list {
   display: flex;
   flex-wrap: wrap;
   background-color: aliceblue;
@@ -45,12 +54,13 @@
   background-color: white;
   border: 1px solid #ccc;
   border-radius: .5rem;
-  box-shadow: 0 0 1rem #0004; 
+  box-shadow: 0 0 1rem #0004;
 }
 
 .price {
   font-size: 2em;
-  font-weight: ;
-  margin-right: ;
+  font-weight: bold;
+  margin-right: .5rem;
 }
+
 </style>
