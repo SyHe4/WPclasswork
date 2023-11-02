250bcb1a22f701410b77b0c2267d57aede0a639b
SyHe4 <henixs1@newpaltz.edu>
2023-11-02 15:19:41 -0400

    filling out api

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { getProducts, getProductById, search } = require('../models/products');
+const { getAll, get, search, create, update, remove } = require('../models/products');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
 
-    res.send(getProducts());
+    res.send(getAll());
 
 })
 .get('/search' , (req, res, next) => {
@@ -15,13 +15,26 @@ router.get('/', (req, res, next) => {
 })
 .get('/:id', (req, res, next) => {
 
-  const product = getProductById(+req.params.id);
+  const product = get(+req.params.id);
   if(product) {
     res.send( product );
   }else {
     res.status(404).send({error: 'Product not found'});
   }
-
 })
+.post('/', (req, res, next) => {
+  const product = create(req.body);
+  res.send(product);
+})
+.patch('/:id', (req, res, next) => {
+  req.body.id = +req.params.id;
+  const product = update(req.body);
+  res.send(product);
+})
+.delete('/:id', (req, res, next) => {
+  remove(+req.params.id);
+  res.send({message: 'Products removed'});
+})
+
 
 module.exports = router;
